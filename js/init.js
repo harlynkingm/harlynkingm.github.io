ce3223b9e02875422dd5f8b0a8f4865fc82b17d8
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-03-18 02:52:05 -0400

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+$(document).ready(function(){
+$('.scrollspy').scrollSpy();
+});
+
+$(document).ready(function(){
+$('.slider').slider({full_width: true});
+});
+   
+        }); // end of document ready
+})(jQuery); // end of jQuery name space
