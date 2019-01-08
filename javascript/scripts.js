361bfa69216a88db975e42e0553f748c20b54cfe
Max Harlynking <mharlynk@andrew.cmu.edu>
2019-01-08 00:26:43 -0800

    Changed movement algorithm

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,8 @@
 var circles = [];
 var speeds = [];
+var colorSpeeds = [];
 var initialYs = [];
+var initialPositions = [];
 var numCircles = 0;
 var maxSize = 0;
 var maxSpeed = 0;
@@ -24,31 +26,58 @@ if (view.size.width < 500) {
 
 for (var i = 0; i < numCircles; i++) {
   var position = Point.random() * view.size;
+  for (var pos = 0; pos < initialPositions.length; pos++) {
+    if (position.getDistance(initialPositions[pos]) < maxSize) {
+      position.x += maxSize
+    }
+  }
   var newInitialY = position.y;
   var radius = Math.random() * (maxSize / 2.0) + (maxSize / 2.0);
   var newCircle = new Path.Circle(position, radius);
-  newCircle.fillColor = {
+  var newColor = {
       hue: Math.random() * 360,
       saturation: 1,
       brightness: 1
   };
-  newCircle.blendMode = 'soft-light';
+  var blurStart = (Math.random() * 0.1) + 0.7;
+  newCircle.fillColor = {
+    gradient: {
+        stops: [[newColor, blurStart], [new Color(255, 255, 255, 0), 1]],
+        radial: true
+    },
+    origin: newCircle.position,
+    destination: newCircle.bounds.rightCenter
+  };
+  newCircle.opacity = (Math.random() * 0.8) + 0.2;
+  newCircle.blendMode = 'overlay';
   var newSpeed = Math.random() * maxSpeed;
   if (newSpeed < 0.05) {
     newSpeed = 0.05;
   }
   var newVector = new Point({
-      angle: 345,
+      angle: (15 * ((Math.random() * 2) - 1)),
       length: newSpeed
   });
+  var newColorSpeed = ((Math.random() * 2) - 1) * 0.01;
+  colorSpeeds.push(newColorSpeed);
   speeds.push(newVector);
   circles.push(newCircle);
   initialYs.push(newInitialY);
+  initialPositions.push(position);
 }
 
 function onFrame() {
 	for (var i = 0, l = circles.length; i < l; i++) {
       circles[i].position += speeds[i];
+      circles[i].opacity += colorSpeeds[i];
+//      if (circles[i].opacity <= 0.02 || circles[i].opacity >= 0.98) {
+//        colorSpeeds[i] *= -1;
+//      }
+      if (circles[i].opacity <= 0.02) {
+        colorSpeeds[i] = Math.random() * 0.01;
+      } else if (circles[i].opacity >= 0.98) {
+        colorSpeeds[i] = Math.random() * -0.01;
+      }
       if (circles[i].position.x > view.bounds.width + maxSize) {
         circles[i].position.x = (maxSize * -1);
         circles[i].position.y = initialYs[i];
